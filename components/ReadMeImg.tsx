752cc1a679437bd83d117dc171c6028caee63c15
Andrew Young <andrew@youngshome.com>
2020-07-25 15:59:37 -0700

    Possible fix to children prop

--- components/ReadMeImg.tsx
+++ components/ReadMeImg.tsx
@@ -1,27 +1,24 @@
 import React from "react";
 
-const ReadmeImg = ({ width, height, children }) => {
-  return (
-    <svg
-      fill="none"
-      width={width}
-      height={height}
-      viewBox={`0 0 ${width} ${height}`}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <foreignObject width={width} height={height}>
-        <div {...{ xmlns: "http://www.w3.org/1999/xhtml" }}>
-          <style>{`
-              * {
-                margin: 0;
-                box-sizing: border-box;
-              }
-            `}</style>
-          {children}
-        </div>
-      </foreignObject>
-    </svg>
-  );
-};
+const ReadMeImg: React.StatelessComponent<{ width: String, height: String }> = ({ children, width, height }) => (
+  <svg
+    fill="none"
+    width={width}
+    height={height}
+    viewBox={`0 0 ${width} ${height}`}
+    xmlns="http://www.w3.org/2000/svg">
+    <foreignObject width={width} height={height}>
+      <div {...{ xmlns: "http://www.w3.org/1999/xhtml" }}>
+        <style>{`
+            * {
+              margin: 0;
+              box-sizing: border-box;
+            }
+          `}</style>
+        {children}
+      </div>
+    </foreignObject>
+  </svg>
+);
 
-export default ReadmeImg;
+export default ReadMeImg;
